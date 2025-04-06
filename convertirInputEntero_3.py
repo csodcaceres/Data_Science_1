4d8eec5c67e5dd43e20f985aef470db2e7cb6d7d
csodcaceres <cs.odcaceres@gmail.com>
2025-04-06 14:27:33 -0300

    new file:   convertirInputEntero_3.py

---
+++ convertirInputEntero_3.py
@@ -0,0 +1,8 @@
+# Podemos recopilar los dos valores con input y convertir la salida a números enteros con la 
+# función int(). Luego, podemos mostrar el resultado de la resta del primer valor menos el segundo 
+# valor con -.
+
+a = int(input('Ingrese el primer valor: '))
+b = int(input('Ingrese el segundo valor: '))
+
+print( a - b)
