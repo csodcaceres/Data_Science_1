cac6c9e2ede985cdc9804b4f4b7df970baa7953a
csodcaceres <cs.odcaceres@gmail.com>
2025-04-06 17:26:10 -0300

    EC 8

---
+++ estructuraCondicional_8.py
@@ -0,0 +1,14 @@
+# Podemos usar el operador de módulo % para determinar si un número es par o impar. 
+# Si la división entera de un número entre 2 da como resultado 0, entonces es par. 
+# Si no, es impar. Esto se debe a que todos los números pares son divisibles por 2, 
+# por lo que no tienen un residuo en la división.
+
+# Recolectamos los datos
+num = int(input('Ingresa un número: '))
+
+# Verificamos si el número es par según el resultado del módulo
+if num % 2 == 0:
+    print('El número es par.')
+else:
+    print('El número es impar.')
+
