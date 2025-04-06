166c389ca0af1b668b92c5b876026e242a36dd9f
csodcaceres <cs.odcaceres@gmail.com>
2025-04-06 16:32:47 -0300

    Funcion para pasar de mayusculas a minusculas

---
+++ editandoTexto_4.py
@@ -0,0 +1,6 @@
+# Recopilamos una frase utilizando la función input y luego mostramos su valor en minúsculas 
+# utilizando el método lower. Podemos mostrar la salida de esto en un comando print.
+
+frase = input('Ingrese una frase en mayuscula: ')
+
+print(frase.lower())
