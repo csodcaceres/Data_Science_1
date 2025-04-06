80f2c09aa0a9b661a5224d0e496ca6bc8829e364
csodcaceres <cs.odcaceres@gmail.com>
2025-04-06 16:38:43 -0300

    Quitando espacios en blanco

---
+++ editandoTexto_5.py
@@ -0,0 +1,7 @@
+# Para este ejemplo, es interesante definir una frase con espacios al principio y al final de la 
+# frase. Una vez hecho esto, podemos eliminar estos espacios utilizando el método strip y mostrar 
+# el resultado en un comando print.
+
+frase = '  !Hola Python!  '
+
+print(frase.strip())
