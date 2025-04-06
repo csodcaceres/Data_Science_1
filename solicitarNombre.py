f07be106722d9f7e739cdf9967543a5f5a416737
csodcaceres <cs.odcaceres@gmail.com>
2025-04-06 13:20:35 -0300

    new file:   solicitarNombre.py

---
+++ solicitarNombre.py
@@ -0,0 +1,6 @@
+# 1 - Podemos solicitar el nombre con la función input y asignar el resultado de la salida a una 
+# variable. A continuación, podemos imprimir el resultado de la variable en la función print 
+# usando la formatación f-string
+
+nombre = input('Ingrese su nombre: ')
+print(f'¡Hola, {nombre}')
