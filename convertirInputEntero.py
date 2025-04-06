e023aa3ab9f864fe47e380dbd22f13b1f1d1a87a
csodcaceres <cs.odcaceres@gmail.com>
2025-04-06 14:13:58 -0300

    new file:   convertirInputEntero.py

---
+++ convertirInputEntero.py
@@ -0,0 +1,7 @@
+# Podemos recopilar los dos valores con input y convertir la salida a un número entero con la 
+# función int(). Luego, podemos mostrar el resultado de la suma entre las dos entradas con +.
+
+a = int(input('Ingrese el primer valor: '))
+b = int(input('Ingrese el segundi valor: '))
+
+print(a + b)
