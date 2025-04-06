d6f6da0599c2a839a908e540d64af86806cc9c11
csodcaceres <cs.odcaceres@gmail.com>
2025-04-06 14:46:24 -0300

    new file:   convertirInputEntero_6.py

---
+++ convertirInputEntero_6.py
@@ -0,0 +1,7 @@
+# Podemos recopilar el operador y potencia con input y convertir la salida a números enteros con la 
+# función int(). Luego, podemos mostrar el resultado de la exponenciación entre estos dos valores con **.
+
+operador = int(input('Ingrese el valor del operador: '))
+potencia = int(input('Ingrese el valor del potencia: '))
+
+print(operador ** potencia)
