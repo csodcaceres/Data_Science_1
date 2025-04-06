90cbb101c6f777b9b3b98e2fd2a05c6f0db2f6d6
csodcaceres <cs.odcaceres@gmail.com>
2025-04-06 16:29:14 -0300

    Agregando el punto 3 de editndo texto

---
+++ editandoTexto_3.py
@@ -0,0 +1,6 @@
+# Recopilamos una frase utilizando la función input y luego mostramos su valor en mayúsculas 
+# utilizando el método upper. Podemos mostrar la salida de esto en un comando print.
+
+frase = input('Escriba una frase: ')
+
+print(frase.upper())
